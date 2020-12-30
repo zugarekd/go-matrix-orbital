c51077a8399d6d4f555e60b97cdf8b07cf2dd8c0
zugarekd <zugarek@verizon.net>
2020-12-29 20:16:20 -0500

    add clock example

--- display_test.go
+++ display_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/tarm/serial"
 	"testing"
+	"time"
 )
 
 func Test_Write(t *testing.T) {
@@ -12,6 +13,7 @@ func Test_Write(t *testing.T) {
 	}
 
 	display.Open()
+	defer display.Close()
 	display.ClearDisplay()
 	display.Write("LCD2021")
 	display.Close()
@@ -24,8 +26,26 @@ func Test_CursorBlinkOff(t *testing.T) {
 	}
 
 	display.Open()
+	defer display.Close()
 	display.ClearDisplay()
 	display.CursorBlinkOff()
 	display.Write("LCD2021")
-	display.Close()
+}
+
+func Test_Clock(t *testing.T) {
+	display := Display{
+		//Config: serial.Config{Name: "/dev/ttyUSB0", Baud: 19200}, // Linux
+		Config: serial.Config{Name: "COM6", Baud: 19200}, // Windows
+	}
+
+	display.Open()
+	defer display.Close()
+	display.ClearDisplay()
+	display.CursorBlinkOff()
+	for true {
+		display.ClearDisplay()
+		clock := time.Now()
+		display.Write(clock.Format("2006-01-02 15:04:05"))
+		time.Sleep(250 * time.Millisecond)
+	}
 }
